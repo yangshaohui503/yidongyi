f476a0d259287eef3dc7cc14d06e74df796b5e13
jiekechoo <jiekechoo@sectong.com>
2016-10-12 16:07:10 +0800

    更新Swagger配置文件

--- src/main/java/com/sectong/SwaggerConfig.java
+++ src/main/java/com/sectong/SwaggerConfig.java
@@ -1,20 +1,12 @@
 package com.sectong;
 
-import static com.google.common.collect.Lists.newArrayList;
-
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -42,8 +34,8 @@ public class SwaggerConfig {
 	}
 
 	private ApiInfo apiInfo() {
-		ApiInfo apiInfo = new ApiInfo("MobileEasy|移动易 API 手册", "API手册", "0.1.0", "Terms of service",
-				"support@mobileeasy.cc", "Apache 2.0", "");
+		ApiInfo apiInfo = new ApiInfo("移动易 API 手册", "API手册", "0.1.0", "Terms of service", "support@sectong.com",
+				"Apache 2.0", "");
 		return apiInfo;
 	}
 
